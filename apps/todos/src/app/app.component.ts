01abd7ea60c930e813ac65ab881def0322a13cbd
Wesley DuSell <dusellfam@gmail.com>
2022-10-02 21:05:12 -0400

    getRecursive

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
-import { AppService, Employee } from './app.service';
+import { expand, flatMap, forkJoin, iif, map, merge, mergeMap, Observable, of, partition, switchMap, tap, toArray } from 'rxjs';
+import { AppService, Employee, EmployeeProfile } from './app.service';
 
 @Component({
   selector: 'tmdjr-root',
@@ -10,42 +10,63 @@ import { AppService, Employee } from './app.service';
 export class AppComponent implements OnInit {
   title = 'todos';
 
-  employee!: Employee[];
-  managers!: Employee[];
+  // employee!: Employee[];
+  // managers!: Employee[];
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.appService.getEmployees().subscribe((employees) => {
-      console.log({ getEployees: employees });
-      this.employee = employees;
-    });
+    this.getRecursive(12).subscribe(console.log);
+    // this.appService.getEmployees().subscribe((employees) => {
+    //   console.log({ getEployees: employees });
+    //   this.employee = employees;
+    // });
 
-    this.appService.getEmployeeById(1).subscribe((employee) => {
-      console.log({ getEmployeeById: employee });
-    });
+    // this.appService.getEmployeeById(1).subscribe((employee) => {
+    //   console.log({ getEmployeeById: employee });
+    // });
 
-    this.appService.getEmployeesByManagerId(2).subscribe((employees) => {
-      console.log({ getEmployeesByManagerId: employees });
-    });
+    // this.appService.getEmployeesByManagerId(2).subscribe((employees) => {
+    //   console.log({ getEmployeesByManagerId: employees });
+    // });
 
-    this.appService.getManagers().subscribe((managers) => {
-      console.log({ getManagers: managers });
-      this.managers = managers;
-    });
+    // this.appService.getManagers().subscribe((managers) => {
+    //   console.log({ getManagers: managers });
+    //   this.managers = managers;
+    // });
     
-    // aggregate the employees by manager
-    this.appService.getManagers().subscribe((managers) => {
-      managers.forEach((manager) => {
-        this.appService.getEmployeesByManagerId(manager.id).subscribe((employees) => {
-          console.log({ manager: manager.name, employees: employees });
-        });
-      });
-    });
+    // // aggregate the employees by manager
+    // this.appService.getManagers().subscribe((managers) => {
+    //   managers.forEach((manager) => {
+    //     this.appService.getEmployeesByManagerId(manager.id).subscribe((employees) => {
+    //       console.log({ manager: manager.name, employees: employees });
+    //     });
+    //   });
+    // });
 
-    this.getManagerAggregateData().subscribe((data) => {
-      console.log({ managerAggregateData: data });
-    });
+    // this.getManagerAggregateData().subscribe((data) => {
+    //   console.log({ managerAggregateData: data });
+    // });
+    // this.getEmployeeProfileAggregateData().subscribe((data) => {
+    //   console.log({ employeeProfileAggregateData: data });
+    // });
+  }
+
+  getRecursive(employeeId: number): Observable<unknown> {
+    return this.appService.getEmployeeById(employeeId)
+    .pipe(
+      mergeMap(employee => this.appService.getEmployeesByManagerId(employee?.id ?? 1)),
+      switchMap(employees =>
+        forkJoin(
+          employees.map(employee => {
+            return iif(() => employee?.title === 'Manager',
+            this.getRecursive(employee?.id ?? 1), 
+            of(employee))
+          })
+        )
+      ),
+      toArray()
+    );
   }
   
   // Create a function that returns aggregate data for each manager as an Observable
@@ -55,14 +76,39 @@ export class AppComponent implements OnInit {
   // manager, employeeCount
   // The manager property should be a string
   // The employeeCount property should be a number
-  getManagerAggregateData() {
-    return of(this.managers.map(manager => {
-      return {
-        manager: manager.name,
-        employeeCount: this.appService.getEmployeesByManagerId(manager.id).subscribe((employees) => {
-          return employees.length;
-        })                                                                                                                                                                                                                      
-      }
-    }));
-  }
+  // getManagerAggregateData() {
+  //   return of(this.appService.getManagers()
+  //   .pipe(
+  //     map(manager => {
+  //       return {
+  //         manager: manager.name,
+  //         employeeCount: this.appService.getEmployeesByManagerId(manager.id).subscribe((employees) => {
+  //           return employees.length;
+  //         })                                                                                                                                                                                                                      
+  //       }
+  //     }))
+  //   )
+  // }
+
+  // Create a function that returns aggregate data for each EmployeeProfile under a manager as an Observable
+  // The aggregate data should include the manager's id and an array employees profiles they manage
+  // The aggregate data should be returned as an array of objects
+  // The array of objects should have the following properties
+  // managerId, employeeProfiles
+  // The managerId property should be a number
+  // The employeeProfiles property should be an array of EmployeeProfile objects
+  // getEmployeeProfileAggregateData() {
+  //   return of(this.managers.map(manager => {
+  //     return {
+  //       managerId: manager.id,
+  //       employeeProfiles: this.appService.getEmployeesByManagerId(manager.id).subscribe((employees) => {
+  //         return employees.map(employee => {
+  //           return this.appService.getEmployeeProfileById(employee.id).subscribe((employeeProfile) => {
+  //             return employeeProfile;
+  //           });
+  //         });
+  //       })
+  //     }
+  //   }));
+  // }
 }
--- apps/todos/src/app/app.service.ts
+++ apps/todos/src/app/app.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { forkJoin, map, merge, mergeMap, Observable, of, partition, tap } from 'rxjs';
 
+// Create interface for employee data
 export interface Employee { id: number, title: string, name: string, reportsTo: number | null };
+
+// Create interface for employeeProfile data
+export interface EmployeeProfile { id: number, employeeId: number, firstName: string, lastName: string, email: string, phone: string, address: string, city: string, state: string, zip: string };
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +18,7 @@ export class AppService {
   // The title should be a string
   // The name should be a string
   // The reportsTo should be a number
-  employee: Employee[] = [
+  employees: Employee[] = [
     { id: 1, title: 'Manager', name: 'John Smith', reportsTo: null },
     { id: 2, title: 'Manager', name: 'Jane Doe', reportsTo: 1 },
     { id: 3, title: 'Manager', name: 'John Doe', reportsTo: 2 },
@@ -34,60 +38,106 @@ export class AppService {
     { id: 17, title: 'Employee', name: 'John Smith', reportsTo: 16 },
   ];
 
+  // Create Array of objects called employeeProfile with the following properties
+  // id, employeeId, firstName, lastName, email, phone, address, city, state, zip
+  // The id should be a number
+  // The employeeId should be a number
+  // The firstName should be a string
+  // The lastName should be a string
+  // The email should be a string
+  // The phone should be a string
+  // The address should be a string
+  // The city should be a string
+  // The state should be a string
+  // The zip should be a string
+  employeeProfiles: EmployeeProfile[] = [
+    { id: 1, employeeId: 1, firstName: 'John', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 2, employeeId: 2, firstName: 'Jane', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 3, employeeId: 3, firstName: 'John', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 4, employeeId: 4, firstName: 'Jane', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 5, employeeId: 5, firstName: 'Joe', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 6, employeeId: 6, firstName: 'Joe', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 7, employeeId: 7, firstName: 'Jane', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 8, employeeId: 8, firstName: 'John', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 9, employeeId: 9, firstName: 'Jane', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 10, employeeId: 10, firstName: 'Joe', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 11, employeeId: 11, firstName: 'Joe', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 12, employeeId: 12, firstName: 'John', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 13, employeeId: 13, firstName: 'Jane', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 14, employeeId: 14, firstName: 'Joe', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 15, employeeId: 15, firstName: 'Joe', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 16, employeeId: 16, firstName: 'Jane', lastName: 'Doe', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+    { id: 17, employeeId: 17, firstName: 'John', lastName: 'Smith', email: '', phone: '', address: '', city: '', state: '', zip: '' },
+  ];
+
 
   // Create a function called getEmployees that returns the employee array
   // and returns an observable of the employee array using the of operator
-  getEmployees() {
-    return of(this.employee);
+  getEmployees(): Observable<Employee[]> {
+    return of(this.employees);
+  }
+
+  // Create a function called getEmployees that returns the employee array
+  // and returns an observable of the employee array using the of operator
+  getEmployeeProfiles(): Observable<EmployeeProfile[]> {
+    return of(this.employeeProfiles);
+  }
+
+  // create a function called getEmployeeProfile that that takes an id as a parameter
+  // and gets EmployeeProfile
+  // getCrewProfile
+  getEmployeeProfileById(id: number): Observable<EmployeeProfile | undefined> {
+    return of(this.employeeProfiles.find(e => e.employeeId === id));
   }
 
   // Create a function called getEmployeeById that takes an id as a parameter
   // and returns observable of the employee object with the matching id
-  getEmployeeById(id: number) {
-    return of(this.employee.find(employee => employee.id === id));
+  getEmployeeById(id: number): Observable<Employee | undefined> {
+    return of(this.employees.find(employee => employee.id === id));
   }
 
   // Create a function called getEmployeesByManagerId that takes an id as a parameter
   // and returns an array of employee objects that have the matching id as their reportsTo property
-  getEmployeesByManagerId(id: number) {
-    return of(this.employee.filter(e => e.reportsTo === id));
+  // getListofDirectReports
+  getEmployeesByManagerId(id: number): Observable<Employee[]> {
+    return of(this.employees.filter(e => e.reportsTo === id));
   }
 
   // Create a function called getManagers that returns an array of employee objects
   // that have a title of 'Manager'
-  getManagers() {
-    return of(this.employee.filter(e => e.title === 'Manager'));
+  getManagers(): Observable<Employee[]> {
+    return of(this.employees.filter(e => e.title === 'Manager'));
   }
 
   // Create a function called getEmployeesByTitle that takes a title as a parameter
   // and returns an array of employee objects that have the matching title
-  getEmployeesByTitle(title: string) {
-    return of(this.employee.filter(e => e.title === title));
+  getEmployeesByTitle(title: string): Observable<Employee[]> {
+    return of(this.employees.filter(e => e.title === title));
   }
 
   // Create a function called getEmployeesWithoutManagers that returns an array of employee objects
   // that have a null value for their reportsTo property
-  getEmployeesWithoutManagers() {
-    return of(this.employee.filter(e => e.reportsTo === null));
+  getEmployeesWithoutManagers(): Observable<Employee[]> {
+    return of(this.employees.filter(e => e.reportsTo === null));
   }
 
   // Create a function called createEmployee that takes an employee object as a parameter
   // and adds the employee object to the employee array
-  createEmployee(employee: Employee) {
-    this.employee.push(employee);
+  createEmployee(employee: Employee): void {
+    this.employees.push(employee);
   }
 
   // Create a function called updateEmployee that takes an employee object as a parameter
   // and updates the employee object in the employee array with the matching id
-  updateEmployee(employee: Employee) {
-    const index = this.employee.findIndex(e => e.id === employee.id);
-    this.employee[index] = employee;
+  updateEmployee(employee: Employee): void {
+    const index = this.employees.findIndex(e => e.id === employee.id);
+    this.employees[index] = employee;
   }
 
   // Create a function called deleteEmployee that takes an id as a parameter
   // and deletes the employee object from the employee array with the matching id
-  deleteEmployee(id: number) {
-    this.employee = this.employee.filter(e => e.id !== id);
+  deleteEmployee(id: number): void {
+    this.employees = this.employees.filter(e => e.id !== id);
   }
 
   // create a function called aggregateEmployeesByManager that returns an array of objects
@@ -98,14 +148,14 @@ export class AppService {
   // The title should be a string
   // The reportsTo should be a number
   // The employees should be an array of employee objects
-  aggregateEmployeesByManager() {
-    return of(this.employee.filter(e => e.reportsTo === null).map(e => {
+  aggregateEmployeesByManager(): Observable<Employee[]> {
+    return of(this.employees.filter(e => e.reportsTo === null).map(e => {
       return {
         id: e.id,
         name: e.name,
         title: e.title,
         reportsTo: e.reportsTo,
-        employees: this.employee.filter(e2 => e2.reportsTo === e.id)
+        employees: this.employees.filter(e2 => e2.reportsTo === e.id)
       }
     }));
   }
